615ad4a913629ec24bb15429a438d99c432ec766
heidariarash <arashheidari94@outlook.com>
2020-09-08 10:30:23 +0430

    second steps of Kmeans

--- classical/unsupervised/clustering/KMeans.py
+++ classical/unsupervised/clustering/KMeans.py
@@ -10,14 +10,15 @@ class KMeans:
         """
         self.k = None
         self.data = None
-        self.centroids = []
-        self.labels = None
-        self.distance = []
+        self.centroids = {}
+        self.labels = []
+        self.distance = {}
 
 
     def train(self, data, k = 2, iter = 10, max_iter = 100):
         """
-        Use this method to train the KMeans algorithm.
+        Usage:
+            trains the KMeans model.
         Inputs:
             data    : data to be trained. it should be a numpy ndarray.
             k       : number of clusters
@@ -30,25 +31,25 @@ class KMeans:
         iteration = 0
 
         #initialising Cetnroids
-        for _ in range(k):
-                self.centroids.append(np.random.randn(*self.data.shape[1:]))
+        idx = np.random.randint(data.shape[0], size= k)
+        self.centroids = data[idx]
 
         #training until centroids don't change or max-iter is reached
         while ( iteration < max_iter and change ):
             for index, point in enumerate(data):
-                self.distance.append([])
-                for center in self.centroids:
-                    self.distance[index].append(np.linalg.norm(point - center))
+                self.distance[index] = []
+                for value in self.centroids:
+                    self.distance[index].append(np.linalg.norm(point - value))
+                self.labels.append(np.argmin(self.distance[index]))
             
+
             iteration +=1
 
         ##to be done:
             #the effect of iter
             #updating centroids
-            #better initialisation
             #save the best cluster of iter clusters
             #write a method to give the elbow data.
-            #updating labels
 
 
 ############## Testing Codes ####################
@@ -56,6 +57,9 @@ kmeans = KMeans()
 data = np.array([[2, 3],
                  [2, 4],
                  [3, 5]])
-kmeans.train(data)
-print(kmeans.distance)
+kmeans.train(data, max_iter=1, k=2)
+# print(kmeans.distance)
+# print(kmeans.centroids)
+# print(kmeans.labels)
+# print(data)
 
