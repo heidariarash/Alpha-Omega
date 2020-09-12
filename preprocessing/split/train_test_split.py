3076f8684327d212170cdc2bd3a3bea37249049b
heidariarash <arashheidari94@outlook.com>
2020-09-12 17:36:08 +0330

    train test config added

--- preprocessing/split/train_test_split.py
+++ preprocessing/split/train_test_split.py
@@ -16,8 +16,53 @@ class TrainTestSplit:
         self.train_data = np.array([])
         self.test_data = np.array([])
         self.validation_data = np.array([])
+        self.__test_rate = 0.3
+        self.__validation = False
+        self.__validation_rate = 0.2
+        self.__random_state = None
 
-    def train(self, features, labels, test_rate = 0.3, validation = False, validation_rate = 0.2):
+    def cofig(self, **kwargs):
+        """
+        Usage: Use this method to configure the TrainTestSplit instantiation.
+
+        Inputs:
+            test_rate      : The percentage of the data that should belong to the test set.
+            validation     : If true, it means you also need validation set.
+            validation_rate: If validation is true, this parameter shows which percentage of the data belongs to the validation set. It will be ignored if validation is False.
+            random_state   : The state of random initializer.
+
+        Returns: Nothing.
+        """
+        if (kwargs["random_state"] is not None):
+            self.__random_state = kwargs["random_state"]
+            
+        if (kwargs["validation"] is not None):
+            self.__validation = kwargs["validation"]
+
+        #checking if the test_rate and validation_rate is in the correct range.
+        if (kwargs["test_rate"] is not None):
+            self.__test_rate = kwargs["test_rate"]
+            if (self.__test_rate < 0 or self.__test_rate > 1.0):
+                print("test_rate could not be less than zero or greater than 1. test_rate reseted to 0.3")
+                self.__test_rate = 0.3
+                return
+            
+
+        if (self.__validation and kwargs["validation_rate"] is not None):
+            self.__validation_rate = kwargs["validarion_rate"]
+            if (self.__validation_rate < 0 or self.__validation_rate > 1.0):
+                print("validation_rate could not be less than zero or greater than 1. validarion_rate reseted to 0.2")
+                self.__validation_rate = 0.2
+                return
+            
+
+        if (self.__validation_rate + self.__test_rate > 1.0):
+            print("validation_rate + test_rate could not be greater than 1. test and validatoin rate reseted to 0.3 and 0.2 respectively.")
+            self.__test_rate = 0.3
+            self.__validation_rate = 0.2
+            return
+
+    def train(self, features, labels):
         """
         Usage : You can use this method to randomly split the data between train, test(, and validation) sets. Trained parameteres are:
             test_idx       : the index of the data which belongs to the test set.
@@ -30,22 +75,7 @@ class TrainTestSplit:
         Inputs:
             features       : The features of the original dataset.
             labels         : The labels of the original dataset.
-            test_rate      : The percentage of the data that should belong to the test set.
-            validation     : If true, it means you also need validation set.
-            validation_rate: If validation is true, this parameter shows which percentage of the data belongs to the validation set.
-            random_state   : The state of random initializer.
 
         Returns: Nothing.
         """
-        #checking if the test_rate and validation_rate is in the correct range.
-        if (test_rate < 0 or test_rate > 1.0):
-            print("test_rate could not be less than zero or greater than 1.")
-            return
-
-        if (validation_rate < 0 or validation_rate > 1.0):
-            print("validation_rate could not be less than zero or greater than 1.")
-            return
-
-        if (validation_rate + test_rate > 1.0):
-            print("validation_rate + test_rate could not be greater than 1.")
-            return
+        
