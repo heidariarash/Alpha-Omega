0b5c6c16265b8b1c14c47efbfa54e24efc585f7f
heidariarash <arashheidari94@outlook.com>
2020-09-14 18:09:50 +0330

    adding kmeans_cost function.

--- alphaomega/ml/classical/unsupervised/clustering/KMeans.py
+++ alphaomega/ml/classical/unsupervised/clustering/KMeans.py
@@ -42,7 +42,7 @@ class KMeans:
                     print("max_iter could not be less than 1. It reseted back to 100.")
                     self.__max_iter = 100
 
-    def train(self, data):
+    def train(self, features):
         """
         Usage: trains the KMeans model. The trained parameteres are:
             labels   : You can check the labels of every data point using this attribute.
@@ -51,11 +51,11 @@ class KMeans:
             distances: If you are curious about the distances of each data point to each centroids you can check it out here.
             
         Inputs:
-            data: data that traines the model. it should be a numpy ndarray (specifically 2darray).
+            features: features that traines the model. it should be a numpy ndarray (specifically 2darray).
             
         Returns: Nothing.
         """
-        if (len(data.shape) != 2):
+        if (len(features.shape) != 2):
             print("Only 2darray")
             return
         
@@ -63,19 +63,19 @@ class KMeans:
         for _ in range(self.__iterations):
             cost = 0
             distance = {}
-            labels = np.array([0] * data.shape[0])
+            labels = np.array([0] * features.shape[0])
             change = True
             iteration = 0
-            new_centroids = np.zeros([self.__k, data.shape[1]])
+            new_centroids = np.zeros([self.__k, features.shape[1]])
 
             #initialising Cetnroids
-            idx = np.random.choice(range(data.shape[0]), self.__k, replace = False)
-            centroids = data[idx]
+            idx = np.random.choice(range(features.shape[0]), self.__k, replace = False)
+            centroids = features[idx]
 
             #training until centroids don't change or max-iter is reached
             while ( iteration < self.__max_iter and change ):
                 #calculating distances between points and centroids
-                for index, point in enumerate(data):
+                for index, point in enumerate(features):
                     distance[index] = []
                     for center in centroids:
                         distance[index].append(np.linalg.norm(point - center))
@@ -84,7 +84,7 @@ class KMeans:
 
                 #updating centroids
                 for center in range(self.__k):
-                    same_label = data[labels == center]
+                    same_label = features[labels == center]
                     new_centroids[center] = np.mean(same_label, axis = 0)
 
                 #checking if the clustering converged and break the loop if true
@@ -95,7 +95,7 @@ class KMeans:
                 iteration +=1
 
             #calculating cost function (sum over the distance between each point to its corresponiding centroid)
-            for index , point in enumerate(data):
+            for index , point in enumerate(features):
                 cost += np.linalg.norm(point - centroids[labels[index]])
 
             #choosing the best clustering based on cost function.
@@ -106,12 +106,12 @@ class KMeans:
                 best_iter = cost
                 
                 
-    def elbow(self, data, min_k = 2, max_k = None, max_iter = 100):
+    def elbow(self, features, min_k = 2, max_k = None, max_iter = 100):
         """
-        Usage: Use this method to evalute KMeans model for different ks.
+        Usage: Use this method to evalute KMeans model for different ks and choose the best k.
         
         Inputs:
-            data    : data for clustering. it should be a numpy ndarray (specifically 2darray).
+            features: features for clustering. it should be a numpy ndarray (specifically 2darray).
             min_k   : minimum number of clusters. default is 2.
             max_k   : maximum number of cluster. If you don't specify a maximum number of cluster, number of data points will be assigend automatically.
             max_iter: number of maximum iteration if the centroids are still not to be found.
@@ -122,35 +122,35 @@ class KMeans:
 
         #check if there is no input for max_k
         if max_k == None:
-            max_k = data.shape[0]
+            max_k = features.shape[0]
 
         #check the boundrary of max_k
-        if max_k > data.shape[0] or max_k < min_k:
+        if max_k > features.shape[0] or max_k < min_k:
             print("maximum number of clusters can not be more than the number of data points.\nmaximum number of clusters can not be less than minimum number of clusters.")
             return
 
         #running the model for each K and compute the cost function and store it in an array.
         for k in range(min_k, max_k + 1):
             self.config(k=k, max_iter = max_iter)
-            self.train(data)
+            self.train(features)
             costs[k] = self.cost
             
         return costs
             
 
-def kmeans_apply(data, k = 2, iterations = 10, max_iter = 100):
+def kmeans_apply(features, k = 2, iterations = 10, max_iter = 100):
     """
     Usage: This function is actually is the same as the class. you can use this API instead of instantiating the KMeans class.
 
     Inputs:
-        data     : data that traines the model. it should be a numpy ndarray (specifically 2darray).
+        features : features that traines the model. it should be a numpy ndarray (specifically 2darray).
         k        : number of clusters
         iteration: number of iteration in total to find the best clusters.
         max_iter : number of maximum iteration if the centroids are still not to be found.
 
-    Returns: a numpy array including the labels for each data label.
+    Returns: a numpy array including the labels for each data point.
     """
-    if (len(data.shape) != 2):
+    if (len(features.shape) != 2):
         print("Only 2darray")
         return -1
 
@@ -158,23 +158,23 @@ def kmeans_apply(data, k = 2, iterations = 10, max_iter = 100):
     for _ in range(iterations):
         cost = 0
         distance = {}
-        labels = np.array([0] * data.shape[0])
+        labels = np.array([0] * features.shape[0])
         change = True
         iteration = 0
-        new_centroids = np.zeros([k, data.shape[1]])
+        new_centroids = np.zeros([k, features.shape[1]])
         
-        idx = np.random.choice(range(data.shape[0]), k, replace = False)
-        centroids = data[idx]
+        idx = np.random.choice(range(features.shape[0]), k, replace = False)
+        centroids = features[idx]
 
         while ( iteration < max_iter and change ):
-            for index, point in enumerate(data):
+            for index, point in enumerate(features):
                 distance[index] = []
                 for center in centroids:
                     distance[index].append(np.linalg.norm(point - center))
                 labels[index] = np.argmin(distance[index])
                 
             for center in range(k):
-                same_label = data[labels == center]
+                same_label = features[labels == center]
                 new_centroids[center] = np.mean(same_label, axis = 0)
             if (new_centroids == centroids).all():
                 change = False
@@ -182,10 +182,46 @@ def kmeans_apply(data, k = 2, iterations = 10, max_iter = 100):
                 centroids = new_centroids.copy()
             iteration +=1
 
-        for index , point in enumerate(data):
+        for index , point in enumerate(features):
             cost += np.linalg.norm(point - centroids[labels[index]])
 
         if cost < best_iter:
             labels_best = labels
             best_iter = cost
     return labels_best
+
+
+def kmeans_cost(features, labels):
+    """
+    Usage: Use this function to find out the cost (summation over distances of every data point to its own centroid)
+
+    Inputs:
+        features: features of the dataset.
+        labels  : labels for each data point in features. This argument is the return parameter of kmeans_apply function.
+    """
+    #checking for the correct shape of features and labels
+    if (len(features.shape) != 2):
+        print("Only 2darray")
+        return
+
+    if (features.shape[0] != len(labels.reshape(-1))):
+        print("Number of data points and labels should be equal.")
+        return
+    
+    #finding the number of clusters
+    k = len(np.unique(labels))
+
+    #initializing centroids and cost
+    centroids = np.zeros([k, features.shape[1]])
+    cost = 0
+
+    #finging the centroids
+    for center in range(k):
+        same_label = features[labels == center]
+        centroids[center] = np.mean(same_label, axis = 0)
+
+    #calculationg cost
+    for index , point in enumerate(features):
+        cost += np.linalg.norm(point - centroids[labels[index]])
+
+    return cost
