79c9347b89690755c1aa42ef8ee84b3cdc3b341f
heidariarash <arashheidari94@outlook.com>
2020-09-16 18:05:03 +0330

    channel merger class complete

--- alphaomega/cv/channel/channel_merge.py
+++ alphaomega/cv/channel/channel_merge.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class ChannelMerge:
+class ChannelMergeer:
     """
     Usage: Use this class to merge different channels of an image into an image.
     """
@@ -10,7 +10,25 @@ class ChannelMerge:
         Inputs : Nothing.
         Returns: An instantiation of the ChannelMerge class.
         """
-        self.__image = None
+        self.__channels_dimension = 2
+
+    def config(self, **kwargs):
+        """
+        Usage: Use this method to configure the parameters of the ChannelMerger instantiation.
+
+        Inputs:
+            channels_dimension: The dimension of the channels. It could be either "first" or "last" (or equally 0 and 2). if it is "first" and you have 3 750x750 channels, the shape of the result is (3,750,750).
+
+        Returns: Nothing.
+        """
+        for key, value in kwargs.items():
+            if key == "channels_dimensions":
+                if value == "first" or value == 0:
+                    self.__channels_dimension = 0
+                elif value == "last" or value == 2:
+                    self.__channels_dimension = 2
+                else:
+                    print("Wrong value for channels_dimension. It could be only 'first' or 'last' (0 and 2 are also possible).")
 
     def apply(self, *channels):
         """
@@ -28,6 +46,8 @@ class ChannelMerge:
         if len(shape) != 2:
             print("Channels should be 2d.")
             return
+        
+        chs = [np.zeros_like(channels[0])] * len(channels)
 
         #checking if all the channels have the same shape. If they have, expanding them.
         for index, channel in enumerate(channels):
@@ -35,7 +55,6 @@ class ChannelMerge:
                 print(f"Channels should be the same size. The channel {index+1} has a different shape as the first channel.")
                 return
             else:
-                np.expand_dims(channel, 0)
-
-        self.__image = np.concatenate(*channels, axis=0)
-        return self.__image
+                chs[index] = np.expand_dims(channel, self.__channels_dimension)
+            
+        return np.concatenate([*chs], axis=self.__channels_dimension)
