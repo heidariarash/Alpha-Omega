bb6d154f13714845d8e7b9c3f95c8b0efe9f1c33
heidariarash <arashheidari94@outlook.com>
2020-09-17 21:50:28 +0330

    mean filter complete

--- alphaomega/cv/kernel/mean_filter.py
+++ alphaomega/cv/kernel/mean_filter.py
@@ -1,4 +1,5 @@
 import numpy as np
+from alphaomega.cv.border.border_intropolation import border_intropolate_apply
 
 class MeanFilter:
     """
@@ -11,7 +12,7 @@ class MeanFilter:
         Returns: An instantiation of MeanFilter Class.
         """
         self.__kernel_size = 3
-        self.__border = "constant"
+        self.__border_type = "constant"
 
     def config(self, **kwargs):
         """
@@ -19,7 +20,7 @@ class MeanFilter:
 
         Inputs:
             kernel_size : The size of the MeanFilter to apply.
-            border      : This parameter determines how to apply filter to the borders. Options are:
+            border_type : This parameter determines how to apply filter to the borders. Options are:
                 "constant": default option.
                 "reflect"
                 "replicate"
@@ -36,24 +37,11 @@ class MeanFilter:
                     print("Please provide an odd number for kernel size.")
                 else:
                     self.__kernel_size = int(value)
-            elif key == "border":
+            elif key == "border_type":
                 if (value not in ["constant", "reflect", "replicate", "wrap", "reflect_without_border"]):
                     print('The only options for border are "constant", "reflect", "replicate", "wrap", and "reflect_without_border".')
                 else:
-                    self.__border = value
-
-    def border_config(self, image, border, num):
-        if border == "constant":
-            image = np.concatenate((np.zeros((image.shape[0], num)), image), axis = 1)
-            image = np.concatenate((image, np.zeros((image.shape[0], num))), axis = 1)
-            image = np.concatenate((np.zeros((num, image.shape[1])), image), axis = 0)
-            image = np.concatenate((image, np.zeros((num, image.shape[1]))), axis = 0)
-            return image
-
-        # if border == "reflect":
-        # if border == "wrap":
-        # if border == "replicate":
-        # if border == "reflect_without_border":
+                    self.__border_type = value
     
     def apply(self, image):
         """
@@ -66,16 +54,70 @@ class MeanFilter:
             - The smoothed image.
         """
         #initializing different parameters
-        filtered_image = np.zeros_like(image, dtype=np.float)
-        coeficient = 1/(self.__kernel_size * self.__kernel_size)
+        filtered_image = np.zeros_like(image, dtype=np.int16)
         half_size = int((self.__kernel_size-1)/2)
 
         #applying border to image
-        image_border = self.border_config(image, self.__border, half_size)
+        image_border = border_intropolate_apply(image, half_size, self.__border_type)
         
         #finding each element of the filtered image.
+        if len(image_border.shape) == 2:
+            for row in range(image.shape[0]):
+                for column in range(image.shape[1]):
+                    filtered_image[row, column] = np.mean( image_border[row : row + 2 * half_size + 1 , column :column + 2*half_size + 1], axis=(0,1))
+
+        elif len(image_border.shape) == 3:
+            for row in range(image.shape[0]):
+                for column in range(image.shape[1]):
+                    filtered_image[ row, column,:] = np.mean( image_border[  row : row + 2 * half_size + 1 , column :column + 2*half_size + 1, :], axis=(0,1))
+
+        return filtered_image
+
+def mean_filter_apply(image, kernel_size, border_type = "constant"):
+    """
+    Usage: Use this function to blur your image using mean filter.
+
+    Inputs:
+        image: The mean filter will be applied on this image.
+        kernel_size : The size of the MeanFilter to apply.
+        border_type : This parameter determines how to apply filter to the borders. Options are:
+            "constant": default option.
+            "reflect"
+            "replicate"
+            "wrap"
+            "reflect_without_border"
+
+    Returns: 
+        - The smoothed image.
+    """
+    if (int(kernel_size) <= 1):
+        print("Kernel size cannot be less than 2.")
+        return
+
+    if (int(kernel_size) %2 == 0):
+        print("Please provide an odd number for kernel size.")
+        return
+
+    if (border_type not in ["constant", "reflect", "replicate", "wrap", "reflect_without_border"]):
+        print('The only options for border are "constant", "reflect", "replicate", "wrap", and "reflect_without_border".')
+        return
+
+    filtered_image = np.zeros_like(image, dtype=np.int16)
+    half_size = int((kernel_size-1)/2)
+
+    #applying border to image
+    image_border = border_intropolate_apply(image, half_size, border_type)
+    
+    #finding each element of the filtered image.
+    if len(image_border.shape) == 2:
         for row in range(image.shape[0]):
             for column in range(image.shape[1]):
-                filtered_image[row, column] = coeficient * np.sum( image_border[row : row + 2 * half_size + 1 , column :column + 2*half_size + 1])
-        
-        return filtered_image
+                filtered_image[row, column] = np.mean( image_border[row : row + 2 * half_size + 1 , column :column + 2*half_size + 1], axis=(0,1))
+
+    elif len(image_border.shape) == 3:
+        for row in range(image.shape[0]):
+            for column in range(image.shape[1]):
+                filtered_image[ row, column,:] = np.mean( image_border[  row : row + 2 * half_size + 1 , column :column + 2*half_size + 1, :], axis=(0,1))
+
+    filtered_image = filtered_image.astype(np.int16)
+    return filtered_image
