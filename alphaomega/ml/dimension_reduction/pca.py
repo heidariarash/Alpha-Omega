24268fade52e239992594369c5f8d825f0ba16d5
heidariarash <arashheidari94@outlook.com>
2020-12-10 18:19:04 +0330

    pca clean

--- alphaomega/ml/dimension_reduction/pca.py
+++ alphaomega/ml/dimension_reduction/pca.py
@@ -1,24 +1,20 @@
 import numpy as np
+from alphaomega.utils.exceptions import WrongDimension, WrongAttribute
 
 class PCA:
     """
     Usage: PCA is a dimensionality reductino technique. Use this class to apply PCA to your features.
     """
     def __init__(self):
-        """
-        Usage  : The constructor of PCA class.
-        Inputs : Nothing.
-        Returns: An instantiation of PCA class.
-        """
-        self.__new_features = 2
+        self.__new_data  = 2
         self.__transform = None
-        self.__explain = None
-        self.__shape = 0
-        self.__mean = 0
-        self.__std = 0
-        self.__applied = False
+        self.__explain   = None
+        self.__shape     = 0
+        self.__mean      = 0
+        self.__std       = 0
+        self.__applied   = False
 
-    def config(self, **kwargs):
+    def config(self, **kwargs: dict) -> None:
         """
         Usage: Use this method to configure the parameters of PCA instantiation.
 
@@ -28,78 +24,74 @@ class PCA:
         Returns: Nothing.
         """
         for key, value in kwargs.items():
-            if key =="new_features":
+            if key == "new_features":
                 if int(value) >= 1:
-                    self.__new_features = int(value)
+                    self.__new_data = int(value)
                 else:
-                    print("The number of new features should be a positive integer.")
+                    raise WrongAttribute("The number of new features should be a positive integer.")
 
-    def train(self, train_features):
+    def train(self, train_data: np.ndarray) -> None:
         """
         Usage: Use this method to train the PCA instantiation.
 
         Inputs:
-            train_features: The train features. Based on these features will The transformation matrix computed.
+            train_data: The train features. Based on these features will The transformation matrix computed.
 
         Returns: Nothing.
         """
-        #checking for the true shape of train_features
-        if len(train_features.shape) != 2:
-            print("The train_features should be tabular (i.e. have 2 dimensions).")
-            return
+        #checking for the true shape of train_data
+        if len(train_data.shape) != 2:
+            raise WrongDimension("The train_data should be tabular (i.e. have 2 dimensions).")
         
-        self.__shape = train_features.shape[1]
+        self.__shape = train_data.shape[1]
 
         #normalizing the featurs
-        self.__mean = np.mean(train_features,axis = 0)
-        self.__std = np.std(train_features, axis = 0)
-        features = (train_features - self.__mean) / (self.__std)
+        self.__mean = np.mean(train_data,axis = 0)
+        self.__std  = np.std(train_data, axis = 0)
+        features    = (train_data - self.__mean) / (self.__std)
 
         #calculating covariance matrix
-        cov_matrix = np.cov(features.T)
+        cov_matrix  = np.cov(features.T)
 
         #calculating eigen vectors and eigen values
         eig_values, eig_vectors = np.linalg.eig(cov_matrix)
         
         self.__transform =  eig_vectors
-        self.__explain = eig_values
-        self.__applied = True
+        self.__explain   = eig_values
+        self.__applied   = True
 
-    def apply(self, features):
+    def apply(self, data: np.ndarray) -> np.ndarray:
         """
-        Usage: Use this method to apply dimensionality reduction to your features.
+        Usage: Use this method to apply dimensionality reduction to your data.
 
         Inputs:
             featuers: The dimensions of these featuers will get reduced.
 
         Returns:
-            - New reduced features.
+            - New reduced data.
         """
-        #checking for the true shape of features
-        if len(features.shape) != 2:
-            print("features should be tabular (i.e. have 2 dimensions).")
-            return
+        #checking for the true shape of data
+        if len(data.shape) != 2:
+            raise WrongDimension("data should be tabular (i.e. have 2 dimensions).")
 
-        if features.shape[1] != self.__shape:
-            print("number of features should be equivalent to the number of features of training data.")
-            return
+        if data.shape[1] != self.__shape:
+            raise WrongDimension("number of data should be equivalent to the number of data of training data.")
 
         #checking if the model is trained.
         if not self.__applied:
-            print("Please train the model first.")
-            return
+            raise Exception("Please train the model first.")
 
-        #nomalizing the features.    
-        features_scaled = (features - self.__mean) / (self.__std)
-        new_feat = np.zeros((features.shape[0], self.__new_features))
+        #nomalizing the data.    
+        data_scaled = (data - self.__mean) / (self.__std)
+        new_feat    = np.zeros((data.shape[0], self.__new_data))
 
         #applying dimension reductino.
-        for i in range(self.__new_features):
-            new_feat[:,i] = features_scaled.dot(self.__transform.T[i])
+        for i in range(self.__new_data):
+            new_feat[:,i] = data_scaled.dot(self.__transform.T[i])
 
         return new_feat
 
-    def explained_variance(self):
+    def explained_variance(self) -> np.ndarray:
         """
         Usage: Use this method to find out the explained variance for each new feature.
 
@@ -110,8 +102,7 @@ class PCA:
         """
         #checking if the model is trained.
         if not self.__applied:
-            print("Please train the model first.")
-            return
+            raise Exception("Please train the model first.")
 
         explained_variances = []
         for i in range(len(self.__explain)):
@@ -120,98 +111,94 @@ class PCA:
         return np.array(explained_variances)
 
 
-def pca_train(train_features):
+def pca_train(train_data: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
     Usage: Use this function to train a pca model.
 
     Inputs:
-        train_features: The PCA algorithm will get trained based on these features.
+        train_data: The PCA algorithm will get trained based on these features.
 
     Returns:
         - The PCA parameters. This parameter is one of the inputs of pca_apply function.
     """
-    #checking for the correct shapes of train_features
-    if len(train_features.shape) != 2:
-        print("train_features should be tabular (i.e. have 2 dimensions).")
-        return
+    #checking for the correct shapes of train_data
+    if len(train_data.shape) != 2:
+        raise WrongDimension("train_data should be tabular (i.e. have 2 dimensions).")
 
-    mean = np.mean(train_features,axis = 0)
-    std = np.std(train_features, axis = 0)
-    scaled_train = (train_features - mean) / (std)
+    mean         = np.mean(train_data,axis = 0)
+    std          = np.std(train_data, axis = 0)
+    scaled_train = (train_data - mean) / (std)
 
     #calculating covariance matrix
-    cov_matrix = np.cov(scaled_train.T)
+    cov_matrix   = np.cov(scaled_train.T)
 
     #calculating eigen vectors and eigen values
     _ , eig_vectors = np.linalg.eig(cov_matrix)
+
     return mean, std, eig_vectors
 
 
-def pca_apply(features, pca_params, new_features = 2):
+def pca_apply(data: np.ndarray, pca_params: tuple[np.ndarray, np.ndarray, np.ndarray], new_features: int = 2):
     """
-    Usage: Use this function to apply PCA to your features.
+    Usage: Use this function to apply PCA to your data.
 
     Inputs:
-        features    : The trained PCA algorihm reduced the dimensions of these featueres.
+        data    : The trained PCA algorihm reduced the dimensions of these featueres.
         pca_params  : The parameters of PCA algorithm. You can obtain this parameters using pca_train function.
-        new_feateurs: The number of new features.
+        new_feateurs: The number of new data.
 
     Returns:
-        - New reduced features.
+        - New reduced data.
     """
     mean, std, eig_vectors = pca_params
-    #checking for the correct shape of features:
-    if len(features.shape) != 2:
-        print("features should be tabular (i.e. have 2 dimensions).")
-        return
+
+    #checking for the correct shape of data:
+    if len(data.shape) != 2:
+        raise WrongDimension("data should be tabular (i.e. have 2 dimensions).")
     
-    if features.shape[1] != eig_vectors.shape[0]:
-        print("number of features should be equivalent to the number of features of training data.")
-        return
+    if data.shape[1] != eig_vectors.shape[0]:
+        raise WrongDimension("number of data should be equivalent to the number of data of training data.")
 
     #checking for the correct value for new_features
     if int(new_features) < 1:
-        print("The number of new features should be a positive integer.")
-        return
+        raise WrongAttribute("The number of new data should be a positive integer.")
 
-    #normalizing the features.
-    scaled_features = (features - mean) / (std)
-
-    new_feat = np.zeros((features.shape[0], new_features))
+    #normalizing the data.
+    scaled_data = (data - mean) / (std)
+    new_feat    = np.zeros((data.shape[0], new_features))
 
     #applying dimension reductino.
     for i in range(new_features):
-        new_feat[:,i] = scaled_features.dot(eig_vectors.T[i])
+        new_feat[:,i] = scaled_data.dot(eig_vectors.T[i])
 
     return new_feat
 
 
-def pca_explained_variance(train_features):
+def pca_explained_variance(train_data: np.ndarray) -> np.ndarray:
     """
     Usage: Use this function to obtain the explained variance of each new feature if you apply PCA algorithm to your featuers.
 
     Inputs:
-        train_features: Explained variance will get calculated based on these featurs.
+        train_data: Explained variance will get calculated based on these featurs.
 
     Returns:
         - A numpy array, each element of that indicates the explained variance by corresponding index feature.
     """
-    #checking for the correct shapes of train_features
-    if len(train_features.shape) != 2:
-        print("train_features should be tabular (i.e. have 2 dimensions).")
-        return
+    #checking for the correct shapes of train_data
+    if len(train_data.shape) != 2:
+        raise WrongDimension("train_data should be tabular (i.e. have 2 dimensions).")
 
-    mean = np.mean(train_features,axis = 0)
-    std = np.std(train_features, axis = 0)
-    scaled_train = (train_features - mean) / (std)
+    mean         = np.mean(train_data,axis = 0)
+    std          = np.std(train_data, axis = 0)
+    scaled_train = (train_data - mean) / (std)
 
     #calculating covariance matrix
-    cov_matrix = np.cov(scaled_train.T)
+    cov_matrix   = np.cov(scaled_train.T)
 
     #calculating eigen vectors and eigen values
-    eig_values , _ = np.linalg.eig(cov_matrix)
-
+    eig_values , _      = np.linalg.eig(cov_matrix)
     explained_variances = []
+    
     for i in range(len(eig_values)):
         explained_variances.append(eig_values[i] / np.sum(eig_values))
 
