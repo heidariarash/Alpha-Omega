14a02aea4926335e37506ef1717cfa5a525fb181
heidariarash <arashheidari94@outlook.com>
2020-12-12 18:03:40 +0330

    histogram clean

--- alphaomega/cv/histogram.py
+++ alphaomega/cv/histogram.py
@@ -1,52 +1,51 @@
 import numpy as np
 from alphaomega.cv.channel.channel_split import channel_splitter_apply
+from alphaomega.utils.exceptions import WrongAttribute, WrongDimension
 
 class Histogram:
     """
     Usage: Use this class to compute histogram of an image.
     """
     def __init__(self):
-        """
-        Usage  : The constructor of Histogram class.
-        Inputs : Nothing.
-        Returns: An instantiation of Histogram class.
-        """
-        self.__image = None
-        self.__bins = 256
-        self.__hist = None
-        self.__shape = 0
-        self.__max_value = 256
+        self.__image     = None
+        self.__bins      = 256
+        self.__hist      = None
+        self.__shape     = 0
+        self.__max_value = 255
         self.__min_value = 0
-        self.__applied = False
+        self.__applied   = False
 
-    def config(self, **kwargs):
+    def config(self, **kwargs) -> None:
         """
         Usage: Use this method to configure different parameteres of Histogram instantiation.
 
         Inputs:
             bins     : Number of bins for histogram.
-            max_value: The maximum value present in histogram of the image. It usually is 256.
+            max_value: The maximum value present in histogram of the image. It usually is 255.
             min_value: The minimum value present in histogram of the imgea. It usually is 0.
 
         Returns: Nothing.
         """
         for key, value in kwargs.items():
+
             if key == "bins":
                 if int(value) <=0:
-                    print("bins should be a positive integer.")
+                    raise WrongAttribute("bins should be a positive integer.")
                 else:
-                    self.__bins = value
+                    self.__bins = int(value)
+
             elif key == "max_value":
                 self.__max_value = value
+
             elif key == "min_value":
                 self.__min_value = value
 
         if self.__min_value >= self.__max_value:
-            print("min_value can not be greater than or equal to max_value. Both reseted to 0 and 256 respectively.")
-            self.__max_value = 256
+            print("min_value can not be greater than or equal to max_value. Both reseted to 0 and 255 respectively.")
+            self.__max_value = 255
             self.__min_value = 0
 
-    def get(self, attribute):
+    def get(self, attribute: str) -> np.ndarray:
         """
         Usage: Use this method to access different attributes of Histogram instantiation.
 
@@ -59,7 +58,7 @@ class Histogram:
             The value of desired attribute.
         """
         if not self.__applied:
-            print("You should use the apply method first, Then using this method is possible.")
+            raise Exception("You should use the apply method first, Then using this method is possible.")
 
         if attribute == "histogram":
             return self.__hist
@@ -67,9 +66,9 @@ class Histogram:
         if attribute == "image":
             return self.__image
 
-        print('Please specify the correct attribute. Options are: "histogram" and "image".')
+        raise WrongAttribute('Please specify the correct attribute. Options are: "histogram" and "image".')
 
-    def apply(self, image):
+    def apply(self, image: np.ndarray) -> np.ndarray:
         """
         Usage: Use this method to calculate the histogram of an image.
 
@@ -90,8 +89,7 @@ class Histogram:
             self.__image = image
 
         else:
-            print("image should be 2D(single channel) or 3D(multi channel).")
-            return
+            raise WrongDimension("image should be 2D(single channel) or 3D(multi channel).")
 
         bins = np.linspace(self.__max_value, self.__min_value, self.__bins + 1)
         if (self.__shape == 2):
@@ -115,7 +113,7 @@ class Histogram:
         return hist
 
 
-def histogram_apply(image, bins = 256, max_value = 255, min_value = 0):
+def histogram_apply(image: np.ndarray, bins: int = 256, max_value: int = 255, min_value: int = 0):
     """
     Usage: Use this function to compute the histogram of an image.
 
@@ -131,13 +129,11 @@ def histogram_apply(image, bins = 256, max_value = 255, min_value = 0):
     """
     #checking fot the correct value of bins.
     if int(bins) <=0:
-        print("bins should be an positive integer.")
-        return
+        raise WrongAttribute("bins should be an positive integer.")
     
     #checking if min_value is actually less than max_value.
     if min_value >= max_value:
-        print("min_value can not be greater than or equal to max_value.")
-        return
+        raise WrongAttribute("min_value can not be greater than or equal to max_value.")
     
     #checking for the correct shape of the image.
     if len(image.shape) == 2 or ( len(image.shape) == 3 and image.shape[2] == 1):
@@ -145,8 +141,7 @@ def histogram_apply(image, bins = 256, max_value = 255, min_value = 0):
     elif len(image.shape) == 3:
         shape = 3
     else:
-        print("image should be 2D(single channel) or 3D(multi channel).")
-        return
+        raise WrongDimension("image should be 2D(single channel) or 3D(multi channel).")
 
     #applying histogramization
     bins = np.linspace(max_value, _min_value, bins + 1)
@@ -156,7 +151,6 @@ def histogram_apply(image, bins = 256, max_value = 255, min_value = 0):
         for index, bin in enumerate(bins[2:]):
             hist[bins - index - 2] = len(image[image >= bin]) - len(image[image>= bins[index + 1]])
 
-        hist = hist
         return hist
 
     channels = channel_splitter_apply(image)
