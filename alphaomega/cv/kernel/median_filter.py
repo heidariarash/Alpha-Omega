ea9c1b49b2126a61abdb7cdc2908cc759a5c44f0
heidariarash <arashheidari94@outlook.com>
2020-12-12 18:12:56 +0330

    median filter clean

--- alphaomega/cv/kernel/median_filter.py
+++ alphaomega/cv/kernel/median_filter.py
@@ -1,20 +1,16 @@
 import numpy as np
 from alphaomega.cv.border.border_intropolation import border_intropolate_apply
+from alphaomega.utils.exceptions import WrongAttribute
 
 class MedianFilter:
     """
     Usage: Use this filter to blur your images using mean filter.
     """
     def __init__(self):
-        """
-        Usage  : The constructor of MedianFilter Class.
-        Inputs : Nothing.
-        Returns: An instantiation of MedianFilter Class.
-        """
         self.__kernel_size = 3
         self.__border_type = "constant"
 
-    def config(self, **kwargs):
+    def config(self, **kwargs) -> None:
         """
         Usage: Use this method to configure the paramteres of MedianFilter instantiation.
 
@@ -30,20 +26,22 @@ class MedianFilter:
         Returns: Nothing.
         """
         for key, value in kwargs.items():
+
             if key == "kernel_size":
                 if (int(value) <= 1):
-                    print("Kernel size cannot be less than 2.")
+                    raise WrongAttribute("Kernel size cannot be less than 2.")
                 elif (int(value) %2 == 0):
-                    print("Please provide an odd number for kernel size.")
+                    raise WrongAttribute("Please provide an odd number for kernel size.")
                 else:
                     self.__kernel_size = int(value)
+
             elif key == "border_type":
                 if (value not in ["constant", "reflect", "replicate", "wrap", "reflect_without_border"]):
-                    print('The only options for border are "constant", "reflect", "replicate", "wrap", and "reflect_without_border".')
+                    raise WrongAttribute('The only options for border are "constant", "reflect", "replicate", "wrap", and "reflect_without_border".')
                 else:
                     self.__border_type = value
     
-    def apply(self, image):
+    def apply(self, image: np.ndarray) -> np.ndarray:
         """
         Usage: Use this method to apply the MedianFilter to your image.
         
@@ -55,7 +53,7 @@ class MedianFilter:
         """
         #initializing different parameters
         filtered_image = np.zeros_like(image, dtype=np.int16)
-        half_size = int((self.__kernel_size-1)/2)
+        half_size      = int((self.__kernel_size-1)/2)
 
         #applying border to image
         image_border = border_intropolate_apply(image, half_size, self.__border_type)
@@ -73,7 +71,8 @@ class MedianFilter:
 
         return filtered_image
 
-def meidan_filter_apply(image, kernel_size, border_type = "constant"):
+
+def meidan_filter_apply(image :np.ndarray, kernel_size: int = 3, border_type: str = "constant") -> np.ndarray:
     """
     Usage: Use this function to blur your image using mean filter.
 
@@ -91,19 +90,16 @@ def meidan_filter_apply(image, kernel_size, border_type = "constant"):
         - The smoothed image.
     """
     if (int(kernel_size) <= 1):
-        print("Kernel size cannot be less than 2.")
-        return
+        raise WrongAttribute("Kernel size cannot be less than 2.")
 
     if (int(kernel_size) %2 == 0):
-        print("Please provide an odd number for kernel size.")
-        return
+        raise WrongAttribute("Please provide an odd number for kernel size.")
 
     if (border_type not in ["constant", "reflect", "replicate", "wrap", "reflect_without_border"]):
-        print('The only options for border are "constant", "reflect", "replicate", "wrap", and "reflect_without_border".')
-        return
+        raise WrongAttribute('The only options for border are "constant", "reflect", "replicate", "wrap", and "reflect_without_border".')
 
     filtered_image = np.zeros_like(image, dtype=np.int16)
-    half_size = int((kernel_size-1)/2)
+    half_size      = int((kernel_size-1)/2)
 
     #applying border to image
     image_border = border_intropolate_apply(image, half_size, border_type)
