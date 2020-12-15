dd6aa825bb5ef65a33596fb5d26cd36b73413918
heidariarash <arashheidari94@outlook.com>
2020-12-15 17:09:27 +0330

    custom_filter_apply complete

--- alphaomega/cv/kernel/filter.py
+++ alphaomega/cv/kernel/filter.py
@@ -10,7 +10,7 @@ class CustomFilter:
     def __init__(self):
         self.__filter      = None
         self.__border_type = "constant"
-        self.__border_interpolation = BorderIntropolation()
+        self.__border_intropolation = BorderIntropolation()
 
     def config(self, **kwargs) -> None:
         """
@@ -63,18 +63,13 @@ class CustomFilter:
         half_size_y    = int((self.__filter.shape[1]) // 2)
 
         #applying border to image
-        self.__border_interpolation.config(top = half_size_x, bottom = half_size_x, right = half_size_y, left = half_size_y, border_type = self.__border_type)
-        bordered_image = self.__border_interpolation.apply(image)
-        print(bordered_image.shape)
-        print(self.__filter.shape)
-        print(half_size_x)
-        print(half_size_y)
+        self.__border_intropolation.config(top = half_size_x, bottom = half_size_x, right = half_size_y, left = half_size_y, border_type = self.__border_type)
+        bordered_image = self.__border_intropolation.apply(image)
 
         #applying filter
         if len(bordered_image.shape) == 2:
             for row in range(image.shape[0]):
                 for column in range(image.shape[1]):
-                    # print(row, column)
                     filtered_image[row, column] = np.sum( np.multiply(bordered_image[row : row + 2 * half_size_x + 1 , column : column + 2 * half_size_y + 1] , self.__filter))
 
         elif len(bordered_image.shape) == 3:
@@ -84,3 +79,36 @@ class CustomFilter:
                     filtered_image[ row, column,:] = np.sum( bordered_image[  row : row + 2 * half_size_x + 1 , column :column + 2*half_size_y + 1, :] * kernel , axis=(0,1))
 
         return filtered_image
+
+
+def custom_filter_apply(image: np.ndarray, kernel: np.ndarray, border_type: str = "constant") -> np.ndarray:
+    #checking for the correct shape of kenel
+    if len(kernel.shape) != 2:
+        raise WrongDimension("kernel should be 2 dimensional.")
+    if kernel.shape[0] % 2 != 1:
+        raise WrongDimension("Dimensions of the kernel should be odd (not even).")
+    if kernel.shape[1] % 2 != 1:
+        raise WrongDimension("Dimensions of the kernel should be odd (not even).")
+
+    filtered_image = np.zeros_like(image, dtype=np.int16)
+    half_size_x    = int((kernel.shape[0]) // 2)
+    half_size_y    = int((kernel.shape[1]) // 2)
+
+    #applying border to image
+    borderintro = BorderIntropolation()
+    borderintro.config(top = half_size_x, bottom = half_size_x, right = half_size_y, left = half_size_y, border_type = border_type)
+    bordered_image = borderintro.apply(image)
+
+    #applying kernel
+    if len(bordered_image.shape) == 2:
+        for row in range(image.shape[0]):
+            for column in range(image.shape[1]):
+                filtered_image[row, column] = np.sum( np.multiply(bordered_image[row : row + 2 * half_size_x + 1 , column : column + 2 * half_size_y + 1] , kernel))
+
+    elif len(bordered_image.shape) == 3:
+        kernel = channel_merger_apply([kernel, kernel, kernel])
+        for row in range(image.shape[0]):
+            for column in range(image.shape[1]):
+                filtered_image[ row, column,:] = np.sum( bordered_image[  row : row + 2 * half_size_x + 1 , column :column + 2*half_size_y + 1, :] * kernel , axis=(0,1))
+
+    return filtered_image
