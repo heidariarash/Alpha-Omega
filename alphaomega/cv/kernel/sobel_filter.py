9c68b7b63c6ac0dde16757dca2d06465d93fc7f8
heidariarash <arashheidari94@outlook.com>
2020-12-18 18:39:06 +0330

    Sobel compelete

--- alphaomega/cv/kernel/sobel_filter.py
+++ alphaomega/cv/kernel/sobel_filter.py
@@ -17,11 +17,11 @@ class Sobel:
 
         Inputs:
             kernel_size: The size of the sobel filter.
-            type       : The type of the filter. It could be one of these three options:
+            kernel_type: The type of the filter. It could be one of these three options:
                 "xy": performs both x and y Sobel filters (default).
                 "x" : performs only x Sobel filter.
                 "y" : performs only y Sobel filter.
-            border_type  : This parameter determines how to apply filter to the borders. Options are:
+            border_type: This parameter determines how to apply filter to the borders. Options are:
                 "constant": default option.
                 "reflect"
                 "replicate"
@@ -37,9 +37,9 @@ class Sobel:
                     raise WrongDimension("Kernel size should be an odd number greater or equal to three.")
                 self.__kernel_size = int(value)
 
-            elif key == "type":
+            elif key == "kernel_type":
                 if value not in ["xy", "x", "y"]:
-                    raise WrongAttribute("type could be only 'x', 'y', and 'xy'.")
+                    raise WrongAttribute("Kernel type could be only 'x', 'y', and 'xy'.")
                 self.__type = value
 
             elif key == "border_type":
@@ -87,4 +87,71 @@ class Sobel:
         answer  = custom_filter_apply(image, sobel_x, self.__border_type)
         answer += custom_filter_apply(image, sobel_y, self.__border_type)
         return answer
-        
+
+
+def sobel_apply(image: np.ndarray, kernel_size: int = 3, kernel_type: str = "xy", border_type: str = "constant") -> np.ndarray:
+    """
+    Usage: Use this function to apply sobel filter to your image.
+
+    Inputs:
+        image      : The sobel filter will be applied on this image.
+        kernel_size: The size of the sobel filter.
+        kernel_type: The type of the filter. It could be one of these three options:
+            "xy": performs both x and y Sobel filters (default).
+            "x" : performs only x Sobel filter.
+            "y" : performs only y Sobel filter.
+        border_type: This parameter determines how to apply filter to the borders. Options are:
+            "constant": default option.
+            "reflect"
+            "replicate"
+            "wrap"
+            "reflect_without_border"
+
+    Returns:
+        - The output of the sobel filter.
+    """
+    #checking for correct kernel size
+    if int(kernel_size) < 3 or int(kernel_size) % 2 == 0:
+        raise WrongDimension("Kernel size should be an odd number greater or equal to three.")
+    kernel_size = int(kernel_size)
+
+    #chekcing for correct type
+    if kernel_type not in ["xy", "x", "y"]:
+        raise WrongAttribute("Kernel type could be only 'x', 'y', and 'xy'.")
+
+    #checking for correct border type
+    if (border_type not in ["constant", "reflect", "replicate", "wrap", "reflect_without_border"]):
+        raise WrongAttribute('The only options for border are "constant", "reflect", "replicate", "wrap", and "reflect_without_border".')
+
+    #calculating sobel x filter
+    sobel_x = np.zeros((kernel_size, kernel_size))
+    p = [(j,i) for j in range(kernel_size) 
+        for i in range(kernel_size) 
+        if not (i == (kernel_size -1)/2. and j == (kernel_size -1)/2.)]
+
+    for j, i in p:
+        j_ = int(j - (kernel_size -1)/2.)
+        i_ = int(i - (kernel_size -1)/2.)
+        sobel_x[j,i] = (i_ )/float(i_*i_ + j_*j_)
+
+    #calculating sobel y filter
+    sobel_y= np.zeros((kernel_size, kernel_size))
+    p = [(j,i) for j in range(kernel_size) 
+        for i in range(kernel_size) 
+        if not (i == (kernel_size -1)/2. and j == (kernel_size -1)/2.)]
+
+    for j, i in p:
+        j_ = int(j - (kernel_size -1)/2.)
+        i_ = int(i - (kernel_size -1)/2.)
+        sobel_y[j,i] = (j_)/float(i_*i_ + j_*j_)
+
+    #applying filter
+    if kernel_type == "x":
+        return custom_filter_apply(image, sobel_x, border_type)
+
+    if kernel_type == "y":
+        return custom_filter_apply(image, sobel_y, border_type)
+    
+    answer  = custom_filter_apply(image, sobel_x, border_type)
+    answer += custom_filter_apply(image, sobel_y, border_type)
+    return answer
