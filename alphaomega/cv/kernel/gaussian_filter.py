53bd91744023158baf4cf57eb3b9f243108db509
heidariarash <arashheidari94@outlook.com>
2020-12-19 17:45:12 +0330

    gaussian filter based on custom filter

--- alphaomega/cv/kernel/gaussian_filter.py
+++ alphaomega/cv/kernel/gaussian_filter.py
@@ -1,7 +1,7 @@
 import numpy as np
-from alphaomega.cv.border.border_intropolation import border_intropolate_apply
 from alphaomega.cv.channel.channel_merge import channel_merger_apply
 from alphaomega.utils.exceptions import WrongAttribute
+from alphaomega.cv.kernel.filter import custom_filter_apply
 
 class GaussianFilter:
     """
@@ -61,31 +61,15 @@ class GaussianFilter:
             - The smoothed image.
         """
         #initializing different parameters
-        filtered_image = np.zeros_like(image, dtype=np.int16)
-        half_size      = int((self.__kernel_size-1)/2)
-
-        #applying border to image
-        image_border = border_intropolate_apply(image, half_size, self.__border_type)
-
-        y, x   = np.ogrid[-half_size:half_size+1, -half_size:half_size+1]
-        kernel = np.exp( -(y*y + x*x) / ( 2. * self.__sigma * self.__sigma ) )
+        half_size  = int((self.__kernel_size-1)/2)
+        y, x       = np.ogrid[-half_size:half_size+1, -half_size:half_size+1]
+        kernel     = np.exp( -(y*y + x*x) / ( 2. * self.__sigma * self.__sigma ) )
         kernel[ kernel < np.finfo(kernel.dtype).eps*kernel.max() ] = 0
         normalizer = kernel.sum()
         if normalizer != 0:
             kernel /= normalizer
         
-        #finding each element of the filtered image.
-        if len(image_border.shape) == 2:
-            for row in range(image.shape[0]):
-                for column in range(image.shape[1]):
-                    filtered_image[row, column] = np.sum( np.multiply(image_border[row : row + 2 * half_size + 1 , column :column + 2*half_size + 1] , kernel))
-
-        elif len(image_border.shape) == 3:
-            kernel = channel_merger_apply([kernel, kernel, kernel])
-            for row in range(image.shape[0]):
-                for column in range(image.shape[1]):
-                    filtered_image[ row, column,:] = np.sum( image_border[  row : row + 2 * half_size + 1 , column :column + 2*half_size + 1, :] * kernel , axis=(0,1))
-
+        filtered_image = custom_filter_apply(image, kernel, self.__border_type)
         return filtered_image
 
 
@@ -117,29 +101,13 @@ def gaussian_filter_apply(image: np.ndarray, kernel_size: int = 3, sigma: float 
         raise WrongAttribute('The only options for border are "constant", "reflect", "replicate", "wrap", and "reflect_without_border".')
 
     #initializing different parameters
-    filtered_image = np.zeros_like(image, dtype=np.int16)
-    half_size      = int((kernel_size-1)/2)
-
-    #applying border to image
-    image_border = border_intropolate_apply(image, half_size, border_type)
-
-    y, x   = np.ogrid[-half_size:half_size+1, -half_size:half_size+1]
-    kernel = np.exp( -(y*y + x*x) / ( 2. * sigma * sigma ) )
+    half_size  = int((kernel_size-1)/2)
+    y, x       = np.ogrid[-half_size:half_size+1, -half_size:half_size+1]
+    kernel     = np.exp( -(y*y + x*x) / ( 2. * sigma * sigma ) )
     kernel[ kernel < np.finfo(kernel.dtype).eps*kernel.max() ] = 0
     normalizer = kernel.sum()
     if normalizer != 0:
         kernel /= normalizer
     
-    #finding each element of the filtered image.
-    if len(image_border.shape) == 2:
-        for row in range(image.shape[0]):
-            for column in range(image.shape[1]):
-                filtered_image[row, column] = np.sum( np.multiply(image_border[row : row + 2 * half_size + 1 , column :column + 2*half_size + 1] , kernel))
-
-    elif len(image_border.shape) == 3:
-        kernel = channel_merger_apply([kernel, kernel, kernel])
-        for row in range(image.shape[0]):
-            for column in range(image.shape[1]):
-                filtered_image[ row, column,:] = np.sum( image_border[  row : row + 2 * half_size + 1 , column :column + 2*half_size + 1, :] * kernel , axis=(0,1))
-
+    filtered_image = custom_filter_apply(image, kernel, border_type)
     return filtered_image
